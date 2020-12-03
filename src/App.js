1c6090fba66b3751f0bd5c7234c1cda63cd534ee
BibianaBalBar <balbuenabibi@gmail.com>
2020-12-03 22:42:39 +0000

    side-bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Library from "./components/Library";
 import data from "./data";
 import './styles/app.css';
 
@@ -19,7 +20,7 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong} 
       />
-      
+      <Library songs={songs}/>
     </div>
   );
 }
