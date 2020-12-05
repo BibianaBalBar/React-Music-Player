89568a4625766bc3bae18997b9db92f8ec275d44
BibianaBalBar <balbuenabibi@gmail.com>
2020-12-05 17:02:05 +0000

    remove useEffect from player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,14 +1,13 @@
-import React, {useEffect} from "react";
+import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faPause, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 // import {playAudio} from "../util";
 
 const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef, setSongInfo, songInfo, songs, setCurrentSong, setSongs }) => { 
-  //UseEffect
-  useEffect(() => {
+  const activeLibraryHandler = (nextPrev) => {
     //Add Active State
     const newSongs = songs.map((song) => {
-      if(song.id === currentSong.id){
+      if(song.id === nextPrev.id){
         return{
           ...song,
           active: true,
@@ -21,7 +20,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef, setSongInfo, s
       }
     });
     setSongs(newSongs);
-  }, [currentSong]);
+  }; 
 
   //Event Handlers
   const playSongHandler = () => {
@@ -46,14 +45,17 @@ const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef, setSongInfo, s
     let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
     if(direction === 'skip-forward'){
       await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+      activeLibraryHandler(songs[(currentIndex + 1) % songs.length]);
     } 
     if(direction === 'skip-back'){
       if((currentIndex - 1) % songs.length === -1){
         await setCurrentSong(songs[songs.length - 1]);
+        activeLibraryHandler(songs[songs.length - 1]);
         if(isPlaying) audioRef.current.play();
         return;
       }
       await setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+      activeLibraryHandler(songs[(currentIndex - 1) % songs.length]);
     }
     if(isPlaying) audioRef.current.play();
   };    
